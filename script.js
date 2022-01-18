74d2f69515e301bdd28cb2d92c83389af987bae0
alternadiva <asomeonelse@gmail.com>
2022-01-18 17:58:49 +0000

    add event listeners

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // DOM
 
-let username = document.getElementById("username");
-let password = document.getElementById("password");
+let username = document.getElementById("username").value;
+let password = document.getElementById("password").value;
 
 let login = document.getElementById("login");
 let cancel = document.getElementById("cancel");
 
 let testUsername = "testuser";
 let testPassword = "mypassword"
+
+// Event listeners
+
+login.addEventListener("click", testLogin());
+cancel.addEventListener("click", resetLogin());
