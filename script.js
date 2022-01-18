e5e3fcb6db136b8c06f66d8ebbf5e17ba43e5d34
alternadiva <asomeonelse@gmail.com>
2022-01-18 18:45:54 +0000

    add function to validate input value

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // DOM
 
-let username = document.getElementById("username").value;
-let password = document.getElementById("password").value;
+let username = document.getElementById("username");
+let password = document.getElementById("password");
 
 let login = document.getElementById("login");
 let cancel = document.getElementById("cancel");
@@ -9,7 +9,21 @@ let cancel = document.getElementById("cancel");
 let testUsername = "testuser";
 let testPassword = "mypassword"
 
+// Login Test
+
+function testLogin() {
+    if (username.value === testUsername && password.value === testPassword) {
+        username.style.background = "var(--correct-background)";
+        password.style.background = "var(--correct-background)";
+    }
+}
+
+
+function resetLogin() {
+
+}
+
 // Event listeners
 
-login.addEventListener("click", testLogin());
-cancel.addEventListener("click", resetLogin());
+login.addEventListener("click", testLogin);
+cancel.addEventListener("click", resetLogin);
