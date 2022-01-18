eca90aec17cba08a626eb3bb47687bcdb11e034f
alternadiva <asomeonelse@gmail.com>
2022-01-18 19:15:26 +0000

    modify function to inspect input with white spaces

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ let password = document.getElementById("password");
 let login = document.getElementById("login");
 let cancel = document.getElementById("cancel");
 
+let warning = document.getElementById("warning");
+
+// Variables
+
 let testUsername = "testuser";
-let testPassword = "mypassword"
+let testPassword = "mypassword";
 
 // Login Test
 
@@ -15,6 +19,31 @@ function testLogin() {
     if (username.value === testUsername && password.value === testPassword) {
         username.style.background = "var(--correct-background)";
         password.style.background = "var(--correct-background)";
+        warning.innerText = "Correct!"
+        warning.style.background = "var(--correct-background)";
+    }
+    else if (username.value.indexOf(' ') >= 0 || password.value.indexOf(' ') >= 0) {
+        if (username.value.indexOf(' ') >= 0 && password.value.indexOf(' ') >= 0) {
+            username.style.background = "var(--space-background)";
+            username.value = "";
+            password.style.background = "var(--space-background)";
+            password.value = "";
+            warning.innerText = "Incorrect. Please, remove the white spaces!"
+            warning.style.background = "var(--space-background)";
+        }
+        else if (username.value.indexOf(' ') >= 0) {
+            username.style.background = "var(--space-background)";
+            username.value = "";
+            warning.innerText = "Incorrect. Please, remove the white spaces!"
+            warning.style.background = "var(--space-background)";
+        }
+        else {
+            password.style.background = "var(--space-background)";
+            password.value = "";
+            warning.innerText = "Incorrect. Please, remove the white spaces!"
+            warning.style.background = "var(--space-background)";
+        }
+        
     }
 }
 
