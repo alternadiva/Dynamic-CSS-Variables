c7b8e1e2896c4dfa69621fd97e1ad1c4d8c9a952
alternadiva <asomeonelse@gmail.com>
2022-01-18 17:53:53 +0000

    add DOM

---
+++ script.js
@@ -0,0 +1,10 @@
+// DOM
+
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+
+let login = document.getElementById("login");
+let cancel = document.getElementById("cancel");
+
+let testUsername = "testuser";
+let testPassword = "mypassword"
